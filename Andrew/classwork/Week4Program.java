cb8c924b626a244ba43e19c0672dad4afe80b4bf
Andrew <63685025+starsweep@users.noreply.github.com>
2025-09-17 15:10:30 -0400

    add lines to InsultMaker5000

--- Andrew/classwork/Week4Program.java
+++ Andrew/classwork/Week4Program.java
@@ -3,19 +3,27 @@ import java.util.Random;
 public class Week4Program {
     public static void main(String[] args) {
         String[] noun = {
+            "you",
             "your mother",
             "your father",
             "that mouse over there",
             "everything you cherish",
-            "Siffrin - protagonist of hit indie game \"In Stars and Time\" -",
+            "Keanu Reeves",
+            "Gordon Ramsay",
             "Blizzard T. Husky",
             "Chuck Norris",
             "a greasy rat",
             "an assortment of strange creatures",
             "a giant bear",
+            "Hatsune Miku",
+            "the president of The United States of America",
+            "an awesome lesbian couple",
             "Samuel Mitchell",
             "Charles Ceccardi",
-            "Mitchell Oneka"
+            "Mitchell Oneka",
+            "Arthur Socia",
+            "Silas Winter",
+            "Christian Zalenski"
         };
         String[] verb = {
             "sudo rm -rf /",
@@ -30,7 +38,15 @@ public class Week4Program {
             "bear witness to",
             "catfish",
             "curse",
-            "insult"
+            "insult",
+            "make a parody of",
+            "marry",
+            "dispose of",
+            "worsen",
+            "mangle",
+            "defenestrate",
+            "blow up",
+            "kiss"
         };
         String[] object = {
             "your computer",
@@ -42,9 +58,21 @@ public class Week4Program {
             "all your money",
             "your CS1131 group",
             "a pot of rancid stew",
+            "the president of The United States of America",
+            "Brazil",
             "your face",
             "your mother",
-            "your father"
+            "your father",
+            "your job",
+            "your family",
+            "your student loans",
+            "you",
+            "Samuel Mitchell",
+            "Charles Ceccardi",
+            "Mitchell Oneka",
+            "Arthur Socia",
+            "Silas Winter",
+            "Christian Zalenski"
         };
         Random generator = new Random();
         System.out.println("May " + noun[generator.nextInt(noun.length)] + " " + verb[generator.nextInt(verb.length)] + " " + object[generator.nextInt(object.length)]);
